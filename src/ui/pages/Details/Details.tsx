a6702ff1125b513590d883198460d9f36c99be24
Mickael Gomes <mickaelgomesconsulting@gmail.com>
2021-10-24 13:53:44 +0200

    Wrapped details page into a Page component

--- src/ui/pages/Details/Details.tsx
+++ src/ui/pages/Details/Details.tsx
@@ -1,12 +1,18 @@
-import { RouteProps, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
+import Page from "../../templates/Page/Page";
 
 interface Params {
   id: string;
 }
 
-const Details = (props: RouteProps) => {
+const Details = () => {
   const { id } = useParams<Params>();
-  return <div>DETAILS FROM {id}</div>;
+
+  return (
+    <Page>
+      <div>DETAILS FROM {id}</div>
+    </Page>
+  );
 };
 
 export default Details;
