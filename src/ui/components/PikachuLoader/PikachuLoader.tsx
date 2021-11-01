73e2dce0a2554132e49da465172ccac618711235
Mickael Gomes <mickaelgomesconsulting@gmail.com>
2021-11-01 21:34:35 +0100

    Refactored PikachuLoader for it to use usePokemonPic instead of duplicating logic

--- src/ui/components/PikachuLoader/PikachuLoader.tsx
+++ src/ui/components/PikachuLoader/PikachuLoader.tsx
@@ -1,10 +1,9 @@
-import { useState, useEffect, useContext } from "react";
+import { useState, useEffect } from "react";
 import pikachuGif from "../../../assets/running-pikachu.gif";
 import pikachuGifHD from "../../../assets/running-pikachu-hd.gif";
 import styles from "./PikachuLoader.module.css";
 import AnimatedText from "../AnimatedText/AnimatedText";
-import ResolutionContext from "../../../context/ResolutionContext";
-import { LOW_RESOLUTION } from "../../../constants/Resolution";
+import { usePokemonPic } from "../../../hooks/usePokemonPic";
 
 const TEXT_UPDATE_INTERVAL_MS = 4000;
 
@@ -17,7 +16,7 @@ const LoadingTexts = [
 
 const PikachuLoader = () => {
   const [loadingText, setLoadingText] = useState(LoadingTexts[0]);
-  const { resolution } = useContext(ResolutionContext);
+  const gif = usePokemonPic(pikachuGif, pikachuGifHD);
 
   const generateNextIndex = () => {
     const areTextsTheSame = (text: string) => text === loadingText;
@@ -39,12 +38,10 @@ const PikachuLoader = () => {
 
   useEffect(triggerInterval, [loadingText]);
 
-  const gifToUse = resolution === LOW_RESOLUTION ? pikachuGif : pikachuGifHD;
-
   return (
     <div id="loading-screen" className={styles.container}>
       <div>
-        <img src={gifToUse} alt="pika pika" />
+        <img src={gif} alt="pika pika" />
         <AnimatedText text={loadingText} />
       </div>
     </div>
