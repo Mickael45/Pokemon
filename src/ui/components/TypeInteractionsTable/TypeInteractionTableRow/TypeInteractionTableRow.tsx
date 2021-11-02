c1a9157c70b4af06ed8e8290d73b9e9b9d9e2a7b
Mickael Gomes <mickaelgomesconsulting@gmail.com>
2021-11-02 11:35:50 +0100

    Made render conditional to only render types when neeeded

--- src/ui/components/TypeInteractionsTable/TypeInteractionTableRow/TypeInteractionTableRow.tsx
+++ src/ui/components/TypeInteractionsTable/TypeInteractionTableRow/TypeInteractionTableRow.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { capitalizeFirstLetter } from "../../../../utils/stringManipulation";
 import PokemonType from "../../PokemonType/PokemonType";
 import styles from "./TypeInteractionTableRow.module.css";
@@ -8,16 +9,20 @@ interface IProps {
 }
 
 const TypeInteractionTableRow = ({ type, typeInteractions }: IProps) => {
+  const [checked, setChecked] = useState(false);
+
   const renderDataTag = ({ type, effectiveness }: any) => (
     <PokemonType key={type} type={type}>{` (x${effectiveness})`}</PokemonType>
   );
 
-  const renderDataTags = () => typeInteractions.map(renderDataTag);
+  const renderDataTags = () => (checked ? typeInteractions.map(renderDataTag) : null);
+
+  const handleChange = () => setChecked(!checked);
 
   return (
     <div className={styles.container}>
       <label htmlFor={type}>{type.split(",").map(capitalizeFirstLetter).join(",")} (Weaknesses)</label>
-      <input id={type} type="checkbox" />
+      <input onChange={handleChange} id={type} type="checkbox" checked={checked} />
       <div>{renderDataTags()}</div>
     </div>
   );
