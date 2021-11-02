f87c717d226845b5a7f30b2d8e156f39c15313c1
Mickael Gomes <mickaelgomesconsulting@gmail.com>
2021-11-02 14:05:34 +0100

    Fixed minor bug with pokemon names

--- src/utils/pokemonFormatter/extractors.ts
+++ src/utils/pokemonFormatter/extractors.ts
@@ -7,7 +7,7 @@ const findEnglishEntry = (entry: FlavorTextEntry | GeneraEntry) => entry.languag
 export const replaceBrokenName = (name: string) => brokenNamesMap[name] || name;
 
 export const revertPokemonNameToOriginal = (pokemon: IPokemonResponseType) => {
-  const entry = Object.values(brokenNamesMap).find((entry: string) => pokemon.name === entry);
+  const entry = Object.entries(brokenNamesMap).find((entry: [string, string]) => pokemon.name === entry[1]);
   const name = entry ? entry[0] : "";
 
   return name ? { ...pokemon, name } : pokemon;
