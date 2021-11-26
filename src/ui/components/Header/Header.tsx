7fcc9b84e87dd4e59ddf7e769ec27dcff16b0c87
Mickael Gomes <mickaelgomesconsulting@gmail.com>
2021-11-26 21:18:14 +0100

    Changed favicon to pickachu icon

--- src/ui/components/Header/Header.tsx
+++ src/ui/components/Header/Header.tsx
@@ -11,6 +11,7 @@ const Header = ({ title, description }: IProps) => (
     <meta name="viewport" content="initial-scale=1.0, width=device-width" />
     <meta name="description" content={description} />
 
+    <link rel="icon" href="/surprised-pikachu.png" />
     <link rel="preload" href="/fonts/pixelPokemonFont.ttf" as="font" crossOrigin="" />
     <link rel="preload" href="/fonts/hdPokemonFont.woff" as="font" crossOrigin="" />
     <link rel="preload" href="/fonts/hdPokemonFont-bold.woff" as="font" crossOrigin="" />
